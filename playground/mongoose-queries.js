eace096eecbfb7f59e558c287465dd7774fc2952
Lakshya Yadav <lakshyayadav@Lakshyas-MacBook-Air.local>
2017-12-27 09:57:31 +0530

    Add GET / todos/:id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,23 @@
+const mongoose = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '5a42a369ed48b569f6895cf5';
+
+Todo.find({
+	_id: id
+}).then((todos) => {
+	console.log('Todos', todos);
+});
+
+Todo.findOne({
+	_id: id
+}).then((todo) => {
+	console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+	if(!todo) {
+		return console.log('ID not found');
+	}
+	console.log('Todo by ID', todo);
+})
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -30,6 +31,24 @@ app.get('/todos', (req, res) => {
 		res.status(400).send(e);
 	});
 })
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send('Todo not found!');
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(todo) {
+			res.send({todo}); 
+		} else {
+			res.status(404).send('Todo not found!');
+		}
+	}, (e) => {
+		res.status(400).send();
+	});
+});
 
 app.listen(3000, () => {
 	console.log('Started on port 3000');
